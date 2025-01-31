ec5a5b57da47cd6ffca4ada9b733e61741b40f81
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-30 20:43:09 -0500

    new variables added

--- src/Demo1B/Demo1BTest.java
+++ src/Demo1B/Demo1BTest.java
@@ -4,6 +4,18 @@ public class Demo1BTest {
 public static void main(String[]args) {
 		
 		System.out.println("United States Of America");
+		
+		 int num = 10;
+	        char letter = 'A';
+
+	        if (num > 5) {
+	            System.out.println("Number is greater than 5");
+	        }
+	        
+	        if (letter == 'A') {
+	            System.out.println("Letter is A");
+	        }
+	    }
 }
 
-}
+
